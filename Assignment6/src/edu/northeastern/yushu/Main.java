d68d2b6224db3eb780fd1d063b02af9d50bc3793
yushuwu <wu.yushu1@northeastern.edu>
2022-03-05 12:25:48 -0800

    Assignment 6

--- Assignment6/src/edu/northeastern/yushu/Main.java
+++ Assignment6/src/edu/northeastern/yushu/Main.java
@@ -1,8 +1,6 @@
 package edu.northeastern.yushu;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -141,7 +139,84 @@ public class Main {
         return root;
     }
     // 5.Vertical Order Traversal of a Binary Tree
+    Map<TreeNode, int[]> map = new HashMap<>(); // col, row, val
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        map.put(root, new int[]{0, 0, root.val});
+        dfs(root);
+        List<int[]> list = new ArrayList<>(map.values());
+        Collections.sort(list, (a, b)->{
+            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[1] != b[1]) return a[1] - b[1];
+            return a[2] - b[2];
+        });
+        int n = list.size();
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; ) {
+            int j = i;
+            List<Integer> tmp = new ArrayList<>();
+            while (j < n && list.get(j)[0] == list.get(i)[0]) tmp.add(list.get(j++)[2]);
+            ans.add(tmp);
+            i = j;
+        }
+        return ans;
+    }
+    void dfs(TreeNode root) {
+        if (root == null) return ;
+        int[] info = map.get(root);
+        int col = info[0], row = info[1], val = info[2];
+        if (root.left != null) {
+            map.put(root.left, new int[]{col - 1, row + 1, root.left.val});
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            map.put(root.right, new int[]{col + 1, row + 1, root.right.val});
+            dfs(root.right);
+        }
+    }
     // 6.Count Complete Tree Nodes
+    public int countNodes(TreeNode root) {
+        // exit
+        if (root == null) {
+            return 0;
+        }
+        // divide
+        int l = countNodes(root.left);
+        int r = countNodes(root.right);
+        // merge
+        return l + r + 1;
+    }
     // 7.Sum Root to Leaf Numbers
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    public int helper(TreeNode root, int i){
+        if (root == null) {
+            return 0;
+        }
+        int p = i * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return p;
+        }
+        return helper(root.left, p) + helper(root.right, p);
+    }
     // 8.Delete Leaves With a Given Value
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if(root.left == null && root.right == null && root.val == target) {
+            root = null;
+        }
+
+        return root;
+    }
 }
